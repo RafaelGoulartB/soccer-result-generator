34e51ac82e39919a85b62a8d3f4df0f6084fd024
RafaelGoulartB <rafagoulartb@gmail.com>
2020-12-04 17:23:56 -0300

    feat: add basic layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
+import React, { useState } from 'react'
 import Head from 'next/head'
-import { useState } from 'react'
+import {
+  Link,
+  Flex,
+  Heading,
+  InputGroup,
+  Text,
+  Input,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberDecrementStepper
+} from '@chakra-ui/react'
 
 export default function Home() {
-  const [team1, setTeam1] = useState('')
-  const [team2, setTeam2] = useState('')
+  const [team1, setTeam1] = useState('fef')
+  const [team2, setTeam2] = useState('fefe')
   const [team1goals, setTeam1goals] = useState(0)
   const [team2goals, setTeam2goals] = useState(0)
 
@@ -16,9 +29,65 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {team1 && team1goals && team2 && team2goals && (
-        <a href={href}>Get Image URL</a>
-      )}
+      <Flex flexDir="column" maxW="1200px" w="80%" mx="auto" mt="80">
+        <Heading as="h1" textAlign="center" mx="auto">
+          Soccer Result Image Generator
+        </Heading>
+
+        <Flex>
+          <InputGroup flexDir="column">
+            <Input
+              value={team1}
+              onChange={e => setTeam1(e.target.value)}
+              placeholder="Team 1"
+              size="lg"
+            />
+          </InputGroup>
+
+          <InputGroup flexDir="column">
+            <Input
+              value={team2}
+              onChange={e => setTeam2(e.target.value)}
+              placeholder="Team 1"
+              size="lg"
+            />
+          </InputGroup>
+        </Flex>
+
+        <Flex>
+          <Flex flexDir="column" mr="12">
+            <Text maxW="320px">{team1}</Text>
+            <NumberInput
+              min={0}
+              value={team1goals}
+              onChange={value => setTeam1goals(parseInt(value))}
+            >
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+          </Flex>
+
+          <Flex flexDir="column">
+            <Text maxW="320px">{team2}</Text>
+            <NumberInput
+              min={0}
+              value={team2goals}
+              onChange={value => setTeam2goals(parseInt(value))}
+            >
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+          </Flex>
+        </Flex>
+
+        {team1 && team2 && <Link href={href}>Get Image URL</Link>}
+      </Flex>
     </div>
   )
 }
