c4f870465e4527423f872f2e9f39dedf15ba33ef
RafaelGoulartB <rafagoulartb@gmail.com>
2020-12-04 17:33:50 -0300

    fix: add theme provider to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import React from 'react'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import ThemeContainer from '../contexts/theme/theme-container'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { NextPage } from 'next'
+
+const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <ThemeContainer>
+        <Component {...pageProps} />
+      </ThemeContainer>
+    </>
+  )
 }
 
 export default MyApp
