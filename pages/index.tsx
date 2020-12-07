834ecb06ca664b8fafad200e8bd68192bdc0ca7f
RafaelGoulartB <rafagoulartb@gmail.com>
2020-12-07 08:58:18 -0300

    feat: add goals and teams name to same input group

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
   const [team1goals, setTeam1goals] = useState(0)
   const [team2goals, setTeam2goals] = useState(0)
 
-  const href = `http://localhost:3000/api/game-result.png?teams=${team1}x${team2}&result=${team1goals}x${team2goals}`
+  const href = `http://localhost:3000/api/game-result.png?teams=${team1}-${team2}&result=${team1goals}-${team2goals}`
 
   return (
     <>
@@ -40,63 +40,68 @@ export default function Home() {
           justifyContent="space-between"
           alignItems="center"
         >
-          <Input
-            value={team1}
-            onChange={e => setTeam1(e.target.value)}
-            placeholder="Team 1"
-            w={{ sm: '100%', lg: '48%' }}
-            focusBorderColor="green.200"
-            size="lg"
-            mb={{ sm: '8', lg: '0' }}
-          />
+          <InputGroup flexDir="column" w={{ sm: '100%', lg: '48%' }}>
+            <Input
+              value={team1}
+              onChange={e => setTeam1(e.target.value)}
+              placeholder="Team 1"
+              focusBorderColor="green.200"
+              size="lg"
+              mb={{ sm: '8', lg: '0' }}
+            />
+            <Flex flexDir="column" mt="4">
+              <Text maxW="320px">{team1}</Text>
+              <NumberInput
+                min={0}
+                value={team1goals}
+                onChange={value => setTeam1goals(parseInt(value))}
+              >
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </Flex>
+          </InputGroup>
 
-          <Input
-            value={team2}
-            onChange={e => setTeam2(e.target.value)}
-            placeholder="Team 2"
-            w={{ sm: '100%', lg: '48%' }}
-            focusBorderColor="green.200"
-            size="lg"
-          />
+          <InputGroup flexDir="column" w={{ sm: '100%', lg: '48%' }}>
+            <Input
+              value={team2}
+              onChange={e => setTeam2(e.target.value)}
+              placeholder="Team 2"
+              focusBorderColor="green.200"
+              size="lg"
+              mb={{ sm: '8', lg: '0' }}
+            />
+            <Flex flexDir="column" mt="4">
+              <Text maxW="320px">{team2}</Text>
+              <NumberInput
+                min={0}
+                value={team2goals}
+                onChange={value => setTeam2goals(parseInt(value))}
+              >
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </Flex>
+          </InputGroup>
         </Flex>
 
-        <Flex
-          flexDir={{ sm: 'column', lg: 'row' }}
-          justifyContent="space-around"
-          mt="10"
-        >
-          <Flex flexDir="column">
-            <Text maxW="320px">{team1}</Text>
-            <NumberInput
-              min={0}
-              value={team1goals}
-              onChange={value => setTeam1goals(parseInt(value))}
-            >
-              <NumberInputField />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
-          </Flex>
-
-          <Flex flexDir="column">
-            <Text maxW="320px">{team2}</Text>
-            <NumberInput
-              min={0}
-              value={team2goals}
-              onChange={value => setTeam2goals(parseInt(value))}
-            >
-              <NumberInputField />
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
-            </NumberInput>
-          </Flex>
-        </Flex>
-
-        {team1 && team2 && <Link href={href}>Get Image URL</Link>}
+        {team1 && team2 && (
+          <Link
+            href={href}
+            mt="16"
+            color="green.500"
+            alignSelf="center"
+            textAlign="center"
+          >
+            Get Image URL
+          </Link>
+        )}
       </Flex>
     </>
   )
